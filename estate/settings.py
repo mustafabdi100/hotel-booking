531c70c61d6c1e55daca6665a9e6cfb3011cb1df
mustafabdi100 <106968402+mustafabdi100@users.noreply.github.com>
2023-08-10 18:23:07 +0300

    Update settings.py

--- estate/settings.py
+++ estate/settings.py
@@ -17,9 +17,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-l7%hixn&cjfg9!n20kdq%bpvy7@=*hfione6f*$9igl!hnt_za'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
